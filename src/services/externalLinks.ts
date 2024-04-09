2b1017d57dcc0fb18fb13f2cf8f4298294780bfa
Baran Yancı <73401941+y4nci@users.noreply.github.com>
2024-04-10 02:25:54 +0300

    add rym link

--- src/services/externalLinks.ts
+++ src/services/externalLinks.ts
@@ -3,11 +3,13 @@ import GithubDark from '../assets/images/icons_dark/github.png';
 import LinkedinDark from '../assets/images/icons_dark/linkedin.png';
 import MailDark from '../assets/images/icons_dark/mail.png';
 import METUDark from '../assets/images/icons_dark/metu.png';
+import RYMDark from '../assets/images/icons_dark/rym.png';
 import BehanceLight from '../assets/images/icons_light/behance.png';
 import GithubLight from '../assets/images/icons_light/github.png';
 import LinkedinLight from '../assets/images/icons_light/linkedin.png';
 import MailLight from '../assets/images/icons_light/mail.png';
 import METULight from '../assets/images/icons_light/metu.png';
+import RYMLight from '../assets/images/icons_light/rym.png';
 
 const externalLinkURLs = {
     github: 'https://github.com/y4nci',
@@ -15,6 +17,7 @@ const externalLinkURLs = {
     metu: 'https://user.ceng.metu.edu.tr/~e2449015/',
     mail: 'mailto:baranyanci@gmail.com',
     behance: 'https://www.behance.net/yanci',
+    rym: 'https://rateyourmusic.com/~y4nci',
 };
 
 type ExternalLink = keyof typeof externalLinkURLs;
@@ -37,5 +40,7 @@ export const getIconForExternalLink = (link: ExternalLink, theme: Theme) => {
         return theme === 'light' ? MailLight : MailDark;
     case 'behance':
         return theme === 'light' ? BehanceLight : BehanceDark;
+    case 'rym':
+        return theme === 'light' ? RYMLight : RYMDark;
     }
 };
